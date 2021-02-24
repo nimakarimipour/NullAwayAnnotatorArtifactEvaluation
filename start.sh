229b389f187995c01c9c0464de4757ecaded2d1e
nimakarimipour <karimipour.nima@gmail.com>
2021-02-23 19:14:55 -0800

    update

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # install dependencies locally
 source /var/diagnoser/git.config
 
+
 if [ mvn dependency:get -Dartifact=edu.ucr.cs.riple:nullaway:0.7.12-SNAPSHOT -o -DrepoUrl=file://~/.m2/repository]; then
     :
 else
@@ -43,7 +44,6 @@ else
     popd
     popd
 fi
-
 
 git clone https://${USERNAME}:${KEY}@github.com/nimakarimipour/Docker_AE_NA.git
 cd Docker_AE_NA
