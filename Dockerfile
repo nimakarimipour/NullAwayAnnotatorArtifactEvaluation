697507a3632ec6239a6758a7c1f1610fe513ccf1
nimakarimipour <karimipour.nima@gmail.com>
2021-02-23 16:21:08 -0800

    update

---
+++ Dockerfile
@@ -0,0 +1,64 @@
+FROM openjdk:8-jdk-alpine
+
+LABEL Nima Karimipour <karimipour.nima@gmail.com>
+
+RUN apk add --no-cache curl tar bash procps
+
+ARG MAVEN_VERSION=3.6.3
+ARG USER_HOME_DIR="/root"
+ARG SHA=c35a1803a6e70a126e80b2b3ae33eed961f83ed74d18fcd16909b2d44d7dada3203f1ffe726c17ef8dcca2dcaa9fca676987befeadc9b9f759967a8cb77181c0
+ARG BASE_URL=https://downloads.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries
+
+RUN mkdir -p /usr/share/maven /usr/share/maven/ref \
+  && echo "Downlaoding maven" \
+  && curl -fsSL -o /tmp/apache-maven.tar.gz ${BASE_URL}/apache-maven-${MAVEN_VERSION}-bin.tar.gz \
+  \
+  && echo "Checking download hash" \
+  && echo "${SHA}  /tmp/apache-maven.tar.gz" | sha512sum -c - \
+  \
+  && echo "Unziping maven" \
+  && tar -xzf /tmp/apache-maven.tar.gz -C /usr/share/maven --strip-components=1 \
+  \
+  && echo "Cleaning and setting links" \
+  && rm -f /tmp/apache-maven.tar.gz \
+  && ln -s /usr/share/maven/bin/mvn /usr/bin/mvn
+
+ENV MAVEN_HOME /usr/share/maven
+ENV MAVEN_CONFIG "$USER_HOME_DIR/.m2"
+
+# Downloading and installing Gradle
+# 1- Define a constant with the version of gradle you want to install
+ARG GRADLE_VERSION=6.8.3
+
+# 2- Define the URL where gradle can be downloaded from
+ARG GRADLE_BASE_URL=https://services.gradle.org/distributions
+
+# 3- Define the SHA key to validate the gradle download
+#    obtained from here https://gradle.org/release-checksums/
+ARG GRADLE_SHA=7faa7198769f872826c8ef4f1450f839ec27f0b4d5d1e51bade63667cbccd205
+
+# 4- Create the directories, download gradle, validate the download, install it, remove downloaded file and set links
+RUN mkdir -p /usr/share/gradle /usr/share/gradle/ref \
+  && echo "Downlaoding gradle hash" \
+  && curl -fsSL -o /tmp/gradle.zip ${GRADLE_BASE_URL}/gradle-${GRADLE_VERSION}-bin.zip \
+  \
+  && echo "Checking download hash" \
+  && echo "${GRADLE_SHA}  /tmp/gradle.zip" | sha256sum -c - \
+  \
+  && echo "Unziping gradle" \
+  && unzip -d /usr/share/gradle /tmp/gradle.zip \
+   \
+  && echo "Cleaning and setting links" \
+  && rm -f /tmp/gradle.zip \
+  && ln -s /usr/share/gradle/gradle-${GRADLE_VERSION} /usr/bin/gradle
+
+# 5- Define environmental variables required by gradle
+ENV GRADLE_VERSION 6.8.3
+ENV GRADLE_HOME /usr/bin/gradle
+ENV GRADLE_USER_HOME /cache
+
+ENV PATH $PATH:$GRADLE_HOME/bin
+
+VOLUME $GRADLE_USER_HOME
+
+CMD [""]
---
+++ run.py
@@ -0,0 +1,96 @@
+import json
+import subprocess
+import os
+import threading
+
+PROJECTS_DIR = "/tmp/projects/"
+
+def log(message):
+    print("log: " + message)
+    file_object = open('log.txt', 'a')
+    file_object.write("\n" + str(message))
+    file_object.close()
+
+def delete_file(path):
+    log("Cleaning: " + path)
+    if os.path.exists(path):
+        os.remove(path)
+        log("Cleaned.")
+    else:
+        log("Does not exist.")
+
+def prepare():
+    os.system("chmod +x dependencies.sh && ./dependencies.sh")
+    try:
+        os.makedirs(PROJECTS_DIR)
+        os.makedirs("/tmp/NullAwayFix/")
+    except FileExistsError:
+        log("in prepare: project already exists")    
+    os.system("cp git.key " + PROJECTS_DIR + "/git.key")
+
+def prepare_project(project):
+    log("Cloning project " + project['name'])
+    change_dir = "cd " + PROJECTS_DIR
+    if(not os.path.isdir(PROJECTS_DIR + project['name'] + "/")):
+        command = change_dir + " && git clone " + project['git'] + " < git.key"
+        print("Command: " + command)
+        os.system(command)
+    else:
+        log("Project already exists")
+    delete_file("/tmp/NullAwayFix/fixes.json")
+    delete_file("/tmp/NullAwayFix/diagnose.json")
+    delete_file("/tmp/NullAwayFix/diagnose_report.json")
+
+def commit():
+    log("trying to make a commit")
+    token = open("git.key", "r")
+    os.system("git add .")
+    os.system("git commit -m \"changes comming from docker\"")
+    os.system("git push < git.key")
+
+
+def make_report(project):
+    log("trying to make a report for project: " + str(project['name']))
+
+    change_dir = "cd " + PROJECTS_DIR
+    change_dir += project['path'] + "/"
+    log("changing branch: " + str(change_dir))
+    os.system(change_dir + " && git reset --hard && git checkout diagnose && git reset --hard && git pull")
+    log("finished setting the branch: " + str(change_dir))
+
+    log("building project")
+    os.system(change_dir + " && ./gradlew build")
+
+    log("making diagnose.json")
+    os.system("cp /tmp/NullAwayFix/fixes.json /tmp/NullAwayFix/diagnose.json")
+
+    log("starting diagnose tasks")
+    os.system(change_dir + " && ./gradlew diagnose")
+    log("finished diagnose_report.json")
+
+    log("copying to right results directory")
+    os.system("cp /tmp/NullAwayFix/diagnose_report.json ./results/")
+    os.system("mv ./results/diagnose_report.json ./results/" +
+              project['name'] + ".json")
+
+    log("finished make report request")
+
+def run():
+    with open('./projects.json') as f:
+        projects = json.load(f)
+        for project in projects['projects']:
+            if project['active']:
+                try:
+                    prepare_project(project)
+                    make_report(project)
+                    log("successfully ran the command for project: " +
+                        project['name'])
+                except Exception:
+                    log("something went wrong for: " + project['name'])
+                finally:
+                    log("requesting commit")
+                    commit()
+                    log("finsihed commit")
+                    
+prepare()
+run()
