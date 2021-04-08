9cfafca1ffe7cfe1be91a9a901fe9082aabc2f63
nimakarimipour <karimipour.nima@gmail.com>
2021-04-08 09:16:44 -0700

    changes comming from google cloud

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ PROJECTS_DIR = "/tmp/projects/"
 GIT_USERNAME = str(sys.argv[1])
 GIT_KEY = str(sys.argv[2])
 
-GIT_KEY = "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"
+GIT_KEY = "ghp_cbbl3TZTg6XksbjjjeJIihiH5G8iGFjUg1V2c49"
 GIT_USERNAME = "nimakarimipour"
 
 def log(message):
