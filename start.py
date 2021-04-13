fc0a75da8095d2ba8e11d3f7a99c6e6e489cd135
nimakarimipour <karimipour.nima@gmail.com>
2021-04-13 09:19:21 -0700

    update

--- start.py
+++ start.py
@@ -8,41 +8,41 @@ PROJECTS_DIR = "/tmp/projects/"
 GIT_USERNAME = str(sys.argv[1])
 GIT_KEY = str(sys.argv[2])
 
-def log(message):
-    print("log: " + message)
-    file_object = open('log.txt', 'a')
-    file_object.write("\n" + str(message))
-    file_object.close()
+# def log(message):
+#     print("log: " + message)
+#     file_object = open('log.txt', 'a')
+#     file_object.write("\n" + str(message))
+#     file_object.close()
 
 
 def delete_file(path):
-    log("Cleaning: " + path)
+    print("Cleaning: " + path)
     if os.path.exists(path):
         os.remove(path)
-        log("Cleaned.")
+        print("Cleaned.")
     else:
-        log("Does not exist.")
+        print("Does not exist.")
 
 
 def prepare():
-    log("making directories.")
+    print("making directories.")
     dirs = [PROJECTS_DIR, "/tmp/NullAwayFix/", "./results"]
     for directory in dirs:
         try:
             os.makedirs(PROJECTS_DIR)
         except FileExistsError:
-            log("in prepare: directory already exists: " + directory)
+            print("in prepare: directory already exists: " + directory)
 
 
 def prepare_project(project):
-    log("Preparing project " + project['name'])
+    print("Preparing project " + project['name'])
     change_dir = "cd " + PROJECTS_DIR
     if (not os.path.isdir(PROJECTS_DIR + project['name'] + "/")):
-        log("Project does not exist, cloning now...")
+        print("Project does not exist, cloning now...")
         os.system(change_dir + " && git clone " + project['git'].format(
             GIT_USERNAME, GIT_KEY))
     else:
-        log("Project already exists")
+        print("Project already exists")
     os.system(change_dir + "/" + project['path'] + 
     " && git reset --hard && git checkout docker && git pull")
     os.system(change_dir + "/" + project['path'] + " && git branch -d " + project['branch'])
@@ -50,11 +50,11 @@ def prepare_project(project):
     os.system(change_dir + "/" + project['path'] + " && git checkout -b " + project['branch'] + 
     " && git push --set-upstream origin " + project['branch'])
     os.system("cd /tmp/Diagnoser/ && python3 run.py reset")
-    log("Preparing finished")
+    print("Preparing finished")
 
 
 def commit():
-    log("trying to make a commit")
+    print("trying to make a commit")
     os.system("git pull")
     os.system("git add .")
     os.system("git commit -m \"changes comming from google cloud\"")
@@ -64,40 +64,41 @@ def commit():
 
 
 def autofix(project):
-    log("Autofixing project: " + str(project['name']))
-    log("Preparing config.json...")
+    print("Autofixing project: " + str(project['name']))
+    print("Preparing config.json...")
     config = {
         "PROJECT_PATH": PROJECTS_DIR + project['path'],
         "BUILD_COMMAND": project['build'],
         "INITIALIZE_ANNOT": project['init_annot'],
         "FORMAT": project['format']
     }
-    log("Prepared: " + str(config))
+    print("Prepared: " + str(config))
     with open('/tmp/Diagnoser/config.json', 'w') as outfile:
         json.dump(config, outfile)
-    log("Finished writing.")
+        outfile.close()
+    print("Finished writing.")
     delete_file("/tmp/Docker_AE_NA/pre.out")
     delete_file("/tmp/Docker_AE_NA/loop.out")
-    log("Running autofix (pre)...")
+    print("Running autofix (pre)...")
     os.system("cd /tmp/Diagnoser/ && python3 run.py pre > /tmp/Docker_AE_NA/pre.out")
-    log("Running autofix (pre) finished")
-    log("Running autofix (loop)...")
+    print("Running autofix (pre) finished")
+    print("Running autofix (loop)...")
     os.system("cd /tmp/Diagnoser/ && python3 run.py loop > /tmp/Docker_AE_NA/loop.out")
-    log("Running autofix (loop) finished")
+    print("Running autofix (loop) finished")
     change_path_to_project = "cd " + PROJECTS_DIR + project['path']
     os.system(change_path_to_project + " && git add .")
     os.system(change_path_to_project +
               " && git commit -m \"final result of docker\"")
     os.system(change_path_to_project + " && git push " + project['git'].format(GIT_USERNAME, GIT_KEY))
-    log("Commited changes to project: " + project['name'])
+    print("Commited changes to project: " + project['name'])
 
-    log("Copying infos in results directory.")
+    print("Copying infos in results directory.")
     os.system("cd results/ && rm -rvf " + project['name'])
     os.system("cd results/ && mkdir " + project['name'])
     os.system("cp -r /tmp/NullAwayFix/. " + "results/" + project['name'])
     os.system("mv loop.out " + "results/" + project['name'] + "/loop.out")
     os.system("mv pre.out " + "results/" + project['name'] + "/pre.out")
-    log("Copying finished.")
+    print("Copying finished.")
 
 def run():
     with open('./projects.json') as f:
@@ -108,18 +109,18 @@ def run():
                 try:
                     prepare_project(project)
                     autofix(project)
-                    log("successfully ran the command for project: " +
+                    print("successfully ran the command for project: " +
                         project['name'])
                 except Exception:
-                    log("something went wrong for: " + project['name'])
+                    print("something went wrong for: " + project['name'])
                 finally:
                     end = time.time()
                     file_object = open('time.txt', 'a')
                     file_object.write("\n" + project['name'] + ": " + str(end - start))
                     file_object.close()
-                    log("requesting commit")
+                    print("requesting commit")
                     commit()
-                    log("finsihed commit")
+                    print("finsihed commit")
 
 prepare()
 run()
