016c4347827aeb443e29512529cd30255b59e720
nimakarimipour <karimipour.nima@gmail.com>
2021-04-13 11:03:13 -0700

    update

--- start.py
+++ start.py
@@ -46,9 +46,11 @@ def prepare_project(project):
     os.system(change_dir + "/" + project['path'] + 
     " && git reset --hard && git checkout docker && git pull")
     os.system(change_dir + "/" + project['path'] + " && git branch -d " + project['branch'])
-    os.system(change_dir + "/" + project['path'] + " && git push origin --delete " + project['branch'])
-    os.system(change_dir + "/" + project['path'] + " && git checkout -b " + project['branch'] + 
-    " && git push --set-upstream origin " + project['branch'])
+    os.system(change_dir + "/" + project['path'] + " && git push origin --delete " + project['branch'] + " " + project['git'].format(
+            GIT_USERNAME, GIT_KEY))
+    os.system(change_dir + "/" + project['path'] + " && git checkout -b " + project['branch'])
+    os.system(change_dir + "/" + project['path'] + " && git push --set-upstream origin " + project['branch'] + " " + project['git'].format(
+            GIT_USERNAME, GIT_KEY))
     os.system("cd /tmp/Diagnoser/ && python3 run.py reset")
     log("Preparing finished")
 
