cca443901a17fbb56e37b999bdaf7aec87489e07
nimakarimipour <karimipour.nima@gmail.com>
2021-05-01 12:37:05 -0700

    update

--- models/abstracts/1.java
+++ models/abstracts/1.java
@@ -1,1 +1,12 @@
+class A{
+    @Nullable Field f;
 
+    private LambdaClass f = new LambdaClass() {
+        @Override
+        public void foo() {
+            //derefrence f here;
+        }
+    }
+}
+
+// If check for "f" observed in code
