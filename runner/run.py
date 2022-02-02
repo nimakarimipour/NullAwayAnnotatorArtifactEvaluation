9602e276209613b07e5eb51f323d8ce5d13ffd83
nimakarimipour <karimipour.nima@gmail.com>
2022-02-02 09:46:52 -0800

    Update

--- runner/run.py
+++ runner/run.py
@@ -4,7 +4,7 @@ import time
 
 
 # Run with Python2
-PROJECT_DIR = "/home/nima/Developer/AutoFixer/Evaluation/Projects/{}"
+PROJECT_DIR = "/Users/nima/Developer/NullAwayFixer/Projects/{}"
 DISP = "{} & {} & {} & {}\n"
 CONFIG = {
     "PROJECT_PATH": "/home/nima/Developer/AutoFixer/Evaluation/Projects/libgdx",
@@ -15,7 +15,7 @@ CONFIG = {
         "NULLABLE": "javax.annotation.Nullable"
     },
     "FORMAT": False,
-    "DEPTH": 0
+    "DEPTH": -1
 }
 
 with open('../projects.json') as f:
@@ -26,23 +26,24 @@ with open('../projects.json') as f:
             COMMAND = "cd {} && {}".format(PROJECT_DIR.format(project['path']), {})
             os.system(COMMAND.format("git reset --hard"))
             os.system(COMMAND.format("git checkout docker"))
-            os.system(COMMAND.format("git branch -D paper"))
-            os.system(COMMAND.format("git checkout -b paper"))
-            os.system(COMMAND.format(project['build']))
-            before = len(open("/tmp/NullAwayFix/errors.csv",'r').readlines()) - 1
+            os.system(COMMAND.format("git reset --hard"))
+            # os.system(COMMAND.format("git branch -D paper"))
+            # os.system(COMMAND.format("git checkout -b paper"))
+            # os.system(COMMAND.format(project['build']))
+            # before = len(open("/tmp/NullAwayFix/errors.csv",'r').readlines()) - 1
             config = CONFIG.copy()
             config['PROJECT_PATH'] = PROJECT_DIR.format(project['path'])
             config['BUILD_COMMAND'] = project['build']
             config['ANNOTATION']['INITIALIZE'] = project['annot']['init']
             config['ANNOTATION']['NULLABLE'] = project['annot']['nullable']
-            with open('/home/nima/Developer/AutoFixer/Diagnoser/config.json', 'w') as outfile:
+            with open('/Users/nima/Developer/NullAwayFixer/Scripts/Diagnoser/config.json', 'w') as outfile:
                 json.dump(config, outfile)
             start_time = time.time()
-            os.system("cd /home/nima/Developer/AutoFixer/Diagnoser/ && python3 run.py loop")
-            total_time = time.time() - start_time
-            os.system("mv /tmp/NullAwayFix/log.txt /tmp/NullAwayFix/{}.txt".format(project['name']))
-            os.system(COMMAND.format(project['build']))
-            after = len(open("/tmp/NullAwayFix/errors.csv",'r').readlines()) - 1
-            file1 = open("ans.txt", "a")  # append mode
-            file1.write(DISP.format(project['name'], before, after, int(total_time)))
-            file1.close()
+            os.system("cd /Users/nima/Developer/NullAwayFixer/Scripts/Diagnoser/ && python3 run.py loop")
+            # total_time = time.time() - start_time
+            # os.system("mv /tmp/NullAwayFix/log.txt /tmp/NullAwayFix/{}.txt".format(project['name']))
+            # os.system(COMMAND.format(project['build']))
+            # after = len(open("/tmp/NullAwayFix/errors.csv",'r').readlines()) - 1
+            # file1 = open("ans.txt", "a")  # append mode
+            # file1.write(DISP.format(project['name'], before, after, int(total_time)))
+            # file1.close()
--- sampler/chain/run.py
+++ sampler/chain/run.py
@@ -1,16 +1,18 @@
+from dis import dis
 import imp
 
-
+import random
 import json
 import os
+import re
 
 # Run with Python2
-PROJECT_DIR = "/home/nima/Developer/AutoFixer/Evaluation/Projects/{}"
+PROJECT_DIR = "/Users/nima/Developer/NullAwayFixer/Projects/{}"
 DISP = "{} & {} & {} & {}\n"
 CONFIG = {
-    "PROJECT_PATH": "/home/nima/Developer/AutoFixer/Evaluation/Projects/libgdx",
+    "PROJECT_PATH": "/Users/nima/Developer/NullAwayFixer/Projects/libgdx",
     "BUILD_COMMAND": "./gradlew gdx:build -x test",
-    "ANNOTATION":{
+    "ANNOTATION": {
         "INITIALIZE": "com.badlogic.gdx.Initializer",
         "NONNULL": "javax.annotation.NonNull",
         "NULLABLE": "javax.annotation.Nullable"
@@ -19,41 +21,104 @@ CONFIG = {
     "DEPTH": 0
 }
 
-with open('../projects.json') as f:
-    projects = json.load(f)
-    for project in projects['projects']:
-        project['active'] = True
-        if project['active']:
-            COMMAND = "cd {} && {}".format(PROJECT_DIR.format(project['path']), {})
+
+def convert_json_to_csv(name):
+    f = open('./{}/injected.json'.format(name))
+    fixes = json.load(f)['fixes']
+    keys = "location$*$class$*$method$*$param$*$index$*$uri$*$reason$*$annotation$*$inject".split(
+        "$*$")
+    lines = []
+    for fix in fixes:
+        disp = ""
+        for key in keys:
+            if (key in fix.keys()):
+                val = fix[key]
+                disp += (val if val != None else "null") + "$*$"
+            else:
+                disp += "null" + "$*$"
+        print(disp)
+        lines.append(disp[:-3] + "\n")
+    f = open('./{}/injected.csv'.format(name), "w")
+    f.writelines(lines[:-1])
+    f.close()
 
 
-            os.system(COMMAND.format("git reset --hard"))
-            os.system(COMMAND.format("git checkout base"))
+def remove_reason_field(path):
+    fixes = open(path, 'r').readlines()
+    lines = []
+    for fix in fixes:
+        vals = fix.split("$*$")
+        disp = vals[0] + "$*$" + vals[1] + "$*$" + vals[2] + "$*$" + vals[
+            3] + "$*$" + vals[4] + "$*$" + vals[
+                5] + "$*$" + "null" + "$*$" + vals[7] + "$*$" + vals[
+                    8] + "$*$" + vals[9]
+        lines.append(disp + "\n")
 
-            
+    f = open(path, "w")
+    f.writelines(lines)
+    f.close()
+
+def readErrors(path):
+    lines = open(path, 'r').readlines()
+    index = 0
+    while ("error: [NullAway]" not in lines[index]):
+        index += 1
+    print(index)
+    errors = []
+    while (index < len(lines)):
+        error = ""
+        if ("error: [NullAway]" not in lines[index]):
+            break
+        while ("(see http://t.uber.com/nullaway )" not in lines[index]):
+            error += lines[index]
+            index += 1
+        errors.append(error)
+        index += 1
+    return errors
 
 
+def get_error_fix(path, command):
+    os.system(command + " 2> errors.txt")
+    remove_reason_field("/tmp/NullAwayFix/fixes.csv")
+    fixes = open("/tmp/NullAwayFix/fixes.csv", 'r').readlines()
+    return readErrors(path + "/errors.txt"), fixes
 
-            
+
+def select_sample_errors(COMMAND, project):
+    os.system(COMMAND.format("git reset --hard"))
+    os.system(COMMAND.format("git checkout base"))
+
+    errors_before, fixes = get_error_fix(PROJECT_DIR.format(project['path']),
+                                         COMMAND.format(project['build']))
+
+    os.system(COMMAND.format("git checkout final"))
+
+    errors_after, _ = get_error_fix(PROJECT_DIR.format(project['path']),
+                                    COMMAND.format(project['build']))
+
+    for x in errors_before:
+        if x in errors_after:
+            errors_before.remove(x)
+    selected = random.choices(errors_before, k=5)
+    return selected, fixes
 
 
-            os.system(COMMAND.format("git branch -D paper"))
-            os.system(COMMAND.format("git checkout -b paper"))
-            os.system(COMMAND.format(project['build']))
-            before = len(open("/tmp/NullAwayFix/errors.csv",'r').readlines()) - 1
-            config = CONFIG.copy()
-            config['PROJECT_PATH'] = PROJECT_DIR.format(project['path'])
-            config['BUILD_COMMAND'] = project['build']
-            config['ANNOTATION']['INITIALIZE'] = project['annot']['init']
-            config['ANNOTATION']['NULLABLE'] = project['annot']['nullable']
-            with open('/home/nima/Developer/AutoFixer/Diagnoser/config.json', 'w') as outfile:
-                json.dump(config, outfile)
-            start_time = time.time()
-            os.system("cd /home/nima/Developer/AutoFixer/Diagnoser/ && python3 run.py loop")
-            total_time = time.time() - start_time
-            os.system("mv /tmp/NullAwayFix/log.txt /tmp/NullAwayFix/{}.txt".format(project['name']))
-            os.system(COMMAND.format(project['build']))
-            after = len(open("/tmp/NullAwayFix/errors.csv",'r').readlines()) - 1
-            file1 = open("ans.txt", "a")  # append mode
-            file1.write(DISP.format(project['name'], before, after, int(total_time)))
-            file1.close()
+def run():
+    with open('../projects.json') as f:
+        projects = json.load(f)
+        for project in projects['projects']:
+            project['active'] = True
+            if project['active']:
+                COMMAND = "cd {} && {}".format(
+                    PROJECT_DIR.format(project['path']), {})
+
+                # select sample errors with fixes
+                selected, fixes = select_sample_errors(COMMAND, project)
+
+                # Write selected errors
+                file1 = open('{}/selected.txt'.format(project['path']), 'w')
+                file1.writelines(selected)
+                file1.close()
+
+
+remove_reason_field("/tmp/NullAwayFix/fixes.csv")
