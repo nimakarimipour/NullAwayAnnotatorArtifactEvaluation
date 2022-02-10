377fc40160c3319fcb77220e27d207cd5605aa31
nimakarimipour <karimipour.nima@gmail.com>
2022-02-09 16:39:46 -0800

    update

--- sampler/chain/run.py
+++ sampler/chain/run.py
@@ -1,4 +1,5 @@
 from distutils.command.clean import clean
+from pickle import TRUE
 import random
 import json
 import os
@@ -11,11 +12,68 @@ PROJECT_DIR = "/home/nima/Developer/AutoFixer/Evaluation/Projects/{}" if platfor
 FIX_PATH = "/tmp/NullAwayFix/fixes.csv"
 
 
-def readLines(path):
+def read_lines(path):
     f = open(path, 'r')
     lines = f.readlines()
     f.close()
     return lines
+
+
+def run_autofix(project):
+    os.system("rm /tmp/NullAwayFix/injected.json")
+    CONFIG = {
+        "PROJECT_PATH": "",
+        "BUILD_COMMAND": "",
+        "ANNOTATION": {
+            "INITIALIZE": "",
+            "NONNULL": "",
+            "NULLABLE": ""
+        },
+        "FORMAT": False,
+        "DEPTH": 4
+    }
+    config = CONFIG.copy()
+    config['PROJECT_PATH'] = PROJECT_DIR.format(project['path'])
+    config['BUILD_COMMAND'] = project['build']
+    config['ANNOTATION']['INITIALIZE'] = project['annot']['init']
+    config['ANNOTATION']['NULLABLE'] = project['annot']['nullable']
+    with open('/home/nima/Developer/AutoFixer/Diagnoser/config.json',
+              'w') as outfile:
+        json.dump(config, outfile)
+    os.system(
+        "cd /home/nima/Developer/AutoFixer/Diagnoser/ && python3 run.py loop")
+
+
+def copy_correct_nullaway_config(project):
+    CONFIG = {
+        "INHERITANCE_CHECK_DISABLED": False,
+        "ANNOTATION": {
+            "NONNULL": "UNKNOWN",
+            "NULLABLE": "javax.annotation.Nullable"
+        },
+        "MAKE_CALL_GRAPH": False,
+        "METHOD_PARAM_TEST": {
+            "ACTIVE": False,
+            "INDEX": 10000
+        },
+        "MAKE_METHOD_INHERITANCE_TREE": False,
+        "LOG_ERROR": {
+            "DEEP": False,
+            "ACTIVE": TRUE
+        },
+        "WORK_LIST": "*",
+        "MAKE_FIELD_GRAPH": True,
+        "SUGGEST": {
+            "DEEP": False,
+            "ACTIVE": True
+        },
+        "OPTIMIZED": False
+    }
+    config = CONFIG.copy()
+    config['ANNOTATION']['NULLABLE'] = project['annot']['nullable']
+    with open('/tmp/NullAwayFix/explorer.config',
+              'w') as outfile:
+        json.dump(config, outfile)
 
 
 def convert_json_to_csv(name):
@@ -35,7 +93,16 @@ def convert_json_to_csv(name):
                 disp += "null" + "$*$"
         lines.append(disp[:-3] + "\n")
     #todo for mac
-    lines = [l.replace("$*$///home/nima/Developer/", "$*$file:///home/nima/Developer/", 1) for l in lines]
+    if ("$*$///home/nima/Developer/" in lines[0]):
+        lines = [
+            l.replace("$*$///home/nima/Developer/",
+                      "$*$file:///home/nima/Developer/", 1) for l in lines
+        ]
+    else:
+        lines = [
+            l.replace("$*$//home/nima/Developer/",
+                      "$*$file:///home/nima/Developer/", 1) for l in lines
+        ]
     f = open('./projects/{}/injected.csv'.format(name), "w")
     f.writelines(lines)
     f.close()
@@ -52,7 +119,7 @@ def remove_index_from_error(error):
 
 
 def remove_reason_field(path):
-    fixes = readLines(path)
+    fixes = read_lines(path)
     lines = []
     for fix in fixes:
         vals = fix.split("$*$")
@@ -68,7 +135,7 @@ def remove_reason_field(path):
 
 
 def read_errors(path):
-    lines = readLines(path)
+    lines = read_lines(path)
     index = 0
     if (lines[len(lines) - 1] == '\n'):
         lines = lines[:-1]
@@ -91,7 +158,7 @@ def read_errors(path):
 def get_error_fix(path, command):
     os.system(command + " 2> errors.txt")
     remove_reason_field(FIX_PATH)
-    fixes = readLines(FIX_PATH)
+    fixes = read_lines(FIX_PATH)
     return read_errors(path + "/errors.txt"), fixes[1:]
 
 
@@ -165,19 +232,34 @@ def run():
                 COMMAND = "cd {} && {}".format(
                     PROJECT_DIR.format(project['path']), {})
 
+                # get all fixes
+                run_autofix(project)
+                os.system(
+                    "mv /tmp/NullAwayFix/injected.json ./projects/{}/injected.json"
+                    .format(project['path']))
                 convert_json_to_csv(project['path'])
-                all_fixes = readLines('projects/{}/injected.csv'.format(
+                all_fixes = read_lines('projects/{}/injected.csv'.format(
                     project['path']))
-                
+                os.system(
+                    COMMAND.format(
+                        "git checkout -b final && git commit -m \"Final result\" && git push git push --set-upstream origin final"
+                    ))
+
                 # remove new lines from fixes
-                all_fixes = [f[:-1] if f[len(f) - 1] == '\n' else f for f in all_fixes]
+                all_fixes = [
+                    f[:-1] if f[len(f) - 1] == '\n' else f for f in all_fixes
+                ]
 
                 # reset
                 os.system(COMMAND.format("git reset --hard"))
                 os.system(COMMAND.format("git checkout base"))
 
+                # copy the correct nullaway config
+                copy_correct_nullaway_config(project)
+
                 # select new errors
-                # select_sample_errors(COMMAND, project)
+
+                select_sample_errors(COMMAND, project)
 
                 # select sample errors with fixes
                 selected = read_errors('projects/{}/selected.txt'.format(
@@ -221,11 +303,11 @@ def run():
                             break
                         print("Going for another round...")
                         apply_fixes(to_apply)
-                    
+
                     os.system(
                         COMMAND.format(
-                            "git add . && git commit -m \"Chain Completed\" && git push --set-upstream origin {}".format(
-                                branch)))
+                            "git add . && git commit -m \"Chain Completed\" && git push --set-upstream origin {}"
+                            .format(branch)))
 
 
 run()
